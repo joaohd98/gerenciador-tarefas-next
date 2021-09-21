311696e16e73029eb261981827f59c19dee26bd4
João Damazio <joao.damazio@fiap.com.br>
2021-09-21 20:29:42 -0300

    Add login method

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,19 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {DefaultMessageResponse} from "../../types/default-message-response";
+import {Login} from "../../types/login";
 
-export default (
+const request = (
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<DefaultMessageResponse>
 ) => {
   try {
     if(req.method != "POST") {
-      res.status(400).json({error: "Metodo solicitado não existe"});
-      return;
+      return res.status(400).json({error: "Metodo solicitado não existe"});
     }
 
-    return res.status(200).json({msg: "Usuario logado com sucesso"});
+    const body = req.body as Login;
+
+    if(!body.user || !body.password) {
+      return res.status(400).json({msg: "Login ou senha não inseridos"});
+    }
+
+    if(body.user != "admin@admin.com" || body.password != "123456") {
+      return res.status(400).json({msg: "Usuario não encontrado"});
+    }
+
+    res.status(200).json({msg: "Usuario logado com sucesso"});
   } catch (e) {
     console.log("Ocorreu erro ao autenticar usuário: ", e)
     res.status(500).json({error: "Ocorreu erro ao autenticar usuário"});
   }
 
 }
+
+export default request;
